614983a2d509dcee427318c347e18a5dfaec9b58
Michał Łukasiewicz <m.lukasiewic92@gmail.com>
2023-03-19 16:45:35 +0100

    Test nginx conf-v2

--- projects/web-client/src/environments/environment.prod.ts
+++ projects/web-client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://webservice.dev.hexchess.tk',
-  matchmakingSocket: 'webservice.dev.hexchess.tk/matchmaking',
+  apiUrl: 'https://webservice.dev.hexchess.tk',
+  matchmakingSocket: 'https://webservice.dev.hexchess.tk/matchmaking',
   selectedArmyFallbacksLimit: 2,
   blobStorageUrl: 'https://hexchess-webservice.blackpebble-51ab22b9.westeurope.azurecontainerapps.io',
   avatarsContainerName: 'avatars'
