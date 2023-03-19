3919389839fcc34193a88d7d7514aefa33634c57
Michał Łukasiewicz <m.lukasiewic92@gmail.com>
2023-03-19 15:51:06 +0100

    Add env for production build

--- projects/3d-scene/src/scene/types/map-vector-to-raw-vector.ts
+++ projects/3d-scene/src/scene/types/map-vector-to-raw-vector.ts
@@ -3,5 +3,7 @@ import { IRawVector2 } from "./raw-vector2";
 import { IRawVector3 } from "./raw-vector3";
 
 export type MapVectorToRawVector<T extends object> = {
-  [P in keyof T]: T[P];
+  [P in keyof T] : T[P] extends Vector3 ?
+    IRawVector3 : T[P] extends Vector2 ?
+    IRawVector2 : T[P];
 };
