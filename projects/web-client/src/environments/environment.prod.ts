b69d7127e89d534cf7e12f88885167d664ad709f
Michał Łukasiewicz <m.lukasiewic92@gmail.com>
2023-03-30 15:50:48 +0200

    Change nginx configuration

--- projects/web-client/src/environments/environment.prod.ts
+++ projects/web-client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://webservice.dev.hexchess.tk',
-  matchmakingSocket: 'https://webservice.dev.hexchess.tk/matchmaking',
+  apiUrl: 'https://webservice-dev.hexchess.tk',
+  matchmakingSocket: 'https://webservice-dev.hexchess.tk/matchmaking',
   selectedArmyFallbacksLimit: 2,
   blobStorageUrl: 'https://hexchessstorage.blob.core.windows.net',
   avatarsContainerName: 'avatars'
